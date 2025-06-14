b972c39fc5450cdefe17ed78cb3199b36feb43be
BluePrisoner <dragonemperor9868@gmail.com>
2025-06-14 16:44:15 +0530

    cors

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,19 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const app = express();
 
+const corsOptions = {
+  origin: 'http://localhost:5173', // Your frontend origin
+  credentials: true, // Allow credentials (cookies)
+  optionsSuccessStatus: 200 // Some legacy browsers choke on 204
+};
+
+
+
 
 // Middleware
-app.use(cors());
+app.use(cors(corsOptions));
+
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended: true}))
