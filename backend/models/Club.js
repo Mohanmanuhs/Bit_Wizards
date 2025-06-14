c870b3f5a148e92bc43f4b0c470d5a528f97661b
BluePrisoner <dragonemperor9868@gmail.com>
2025-06-14 19:13:46 +0530

    user club

--- backend/models/Club.js
+++ backend/models/Club.js
@@ -4,6 +4,7 @@ const clubSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   description: String,
   logoUrl: String,
+  userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
   createdAt: { type: Date, default: Date.now },
   isAproved: { type: Boolean, default: false },
 });
--- backend/models/User.js
+++ backend/models/User.js
@@ -5,7 +5,6 @@ const userSchema = new mongoose.Schema({
   email: { type: String, unique: true },
   password: String,
   role: { type: String, enum: ['student', 'club_admin', 'super_admin'], default: 'student' },
-  clubId: { type: mongoose.Schema.Types.ObjectId, ref: 'Club' },
   followingClubs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Club' }],
   createdAt: { type: Date, default: Date.now },
 });
